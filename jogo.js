d3979fb8194956917299e899e3e6e6f0820a9062
Rafael-A8 <83028364+Rafael-A8@users.noreply.github.com>
2022-07-25 16:50:20 -0300

    Posição randomica

--- jogo.js
+++ jogo.js
@@ -9,6 +9,26 @@ function ajustaTamanhoPalcoJogo(){
     console.log(altura, largura)
 
 }
+
 ajustaTamanhoPalcoJogo()
 
-console.log(altura, largura)
+
+function posicaoRandomica() {
+    var  posicaox = Math.floor(Math.random() * largura) - 90
+    var  posicaoy = Math.floor(Math.random() * altura) - 90
+
+    posicaox = posicaox < 0 ? 0 : posicaox
+    posicaoy = posicaoy < 0 ? 0 : posicaoy
+    
+    console.log(posicaox, posicaoy)
+    
+    //criar o elemento html
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosca.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaox + 'px'
+    mosquito.style.top = posicaoy + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
