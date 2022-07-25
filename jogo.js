d25954d6dade909ab00e425bd29e2e867e6e6470
Rafael-A8 <83028364+Rafael-A8@users.noreply.github.com>
2022-07-25 17:15:20 -0300

    Tamanho dos mosquitos

--- jogo.js
+++ jogo.js
@@ -25,10 +25,26 @@ function posicaoRandomica() {
     //criar o elemento html
     var mosquito = document.createElement('img')
     mosquito.src = 'imagens/mosca.png'
-    mosquito.className = 'mosquito1'
+    mosquito.className = tamanhoAleatorio()
     mosquito.style.left = posicaox + 'px'
     mosquito.style.top = posicaoy + 'px'
     mosquito.style.position = 'absolute'
 
     document.body.appendChild(mosquito)
+
+}
+
+function tamanhoAleatorio() {
+    //criar randomicamente o tamanho dos mosquitos
+    var classe = Math.floor(Math.random() * 3)
+
+    switch(classe){
+
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }
 }
